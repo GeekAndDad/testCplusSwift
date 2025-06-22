830fb3076d69eb29c9a631ecc9b5bd572d3611ce
Dad <geekanddad@gmail.com>
2025-06-22 02:54:22 -0700

    clean up

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -11,12 +10,9 @@ let package = Package(
 		.executable(name: "testCmd", targets: ["testCmd"])
 	],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-		
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "testCmd",
             dependencies: [
@@ -27,17 +23,6 @@ let package = Package(
         ),
 		.target(name: "Murmur",
 			dependencies: []
-//				path: nil,
-//				exclude: [],
-//				sources: nil,
-//				resources: nil,
-//				publicHeadersPath: nil, // <#T##String?#>,
-//				packageAccess: false,
-//				cSettings: nil, // <#T##[CSetting]?#>,
-//				cxxSettings: nil, // <#T##[CXXSetting]?#>,
-//				swiftSettings: [.interoperabilityMode(.Cxx)]
-//				linkerSettings: nil, // <#T##[LinkerSetting]?#>,
-//				plugins: nil // <#T##[Target.PluginUsage]?#>
 		)
     ]
 )
